4a1f2b679276e695870a3364de6615c8701bd103
GhazanfarShahbaz <ghazanfar.shahbaz2409@gmail.com>
2020-07-23 00:06:32 -0400

    Scrape module

---
+++ shoe_tab/getShoeData.py
@@ -0,0 +1,54 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+from time import gmtime
+
+
+shoe_brand = {
+    'Nike': 'nike-release-dates',
+    "Air Jordan": 'air-jordan-release-dates',
+    "Adidas": 'adidas-release-dates'
+}
+
+def retrieveData(brand):
+    data = {}
+    data['releases'] = []
+
+    year = gmtime().tm_year
+    month = gmtime().tm_mon
+    yearChange = False
+
+    for z in range(month,month+3):
+        #if z > 12 then that means the year must change
+        if(z > 12):
+            if(not yearChange):
+                year += 1
+                yearChange = True
+            z = z%12
+        
+        #formats month value
+        monthVal = ""
+        if(z < 10):
+            monthVal = f"0{z}"
+        else:
+            monthVal = f"{z}"
+
+        #requests the url
+        response = requests.get(f'https://solecollector.com/sneaker-release-dates/{shoe_brand[brand]}/{year}/{monthVal}/')
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        #gets all the images and shoe data
+        releases = soup.find_all('div', class_= "add-to-calendar")
+        release_images = soup.find_all('div', class_='sneaker-release__img-16x9')
+        
+        #traverses the response and gets adds the name,data and image to a dictionary
+        for x in range(len(releases)):
+            data['releases'].append({
+                "Shoe_Name": releases[x]['data-release-name'],
+                'Release_Date': releases[x]['data-date'][:releases[x]['data-date'].rfind(' ')],
+                'Release_Time':  releases[x]['data-date'][releases[x]['data-date'].rfind(' '):],
+                "Image": str(release_images[x].find_all('img', src = True)[0]['src'])
+
+            })
+    
+    return data['releases']
--- shoe_tab/views.py
+++ shoe_tab/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-
 from .models import shoe_data
 # Create your views here.
-
+from shoe_tab.getShoeData import retrieveData
 
 def index(request):
     "The home page for the shoe tab"
@@ -10,6 +9,6 @@ def index(request):
 
 def shoes(request):
     """Shoe all shoes"""
-    shoes = shoe_data.objects.order_by('date_added')
-    contex        ,gcx     t = {'topics': topics}
-    return render(request, 'shoe_tab/shoe_tab.html', context)
+    # shoes = shoe_data.objects.order_by('date_added')
+    # context = {'topics': topics}
+    return render(request, 'shoe_tab/shoe_tab.html', retrieveData("Nike"))
