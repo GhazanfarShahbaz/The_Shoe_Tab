3130e6fc7a7c9764c004ba67a3b814794edc30bd
GhazanfarShahbaz <ghazanfar.shahbaz2409@gmail.com>
2020-09-12 18:57:18 -0400

    Duplicate fix

--- shoe_tab/getShoeData.py
+++ shoe_tab/getShoeData.py
@@ -20,71 +20,70 @@ def allData():
 
 def gate(brand):
     """Used to see wether or not the calender data needs updating or not"""
-    if gmtime().tm_mday == 1:
-        retrieveData(brand)
-
+    day = gmtime().tm_mday
     year = gmtime().tm_year
     month = gmtime().tm_mon
-    day = gmtime().tm_mday
+    if day == 1 or day == 11 or day == 21:
+        retrieveData(year, month)
     return Shoe.objects.filter(shoe_brand=brand,hyped=False, release_day__gte=day, release_month__gte= month, release_year__gte=year ), Shoe.objects.filter(shoe_brand=brand, hyped = True, release_day__gte=day, release_month__gte= month, release_year__gte=year )
 
-def retrieveData(brand):
+def retrieveData(year, month):
     """Retrieves the shoe data for shoe by brand"""
 
     data = {}
     data['releases'] = []
     data['hyped'] = []
 
-    year = gmtime().tm_year
-    month = gmtime().tm_mon
     yearChange = False
+    for brand in shoe_brand:
+        for months in range(month, month+3):
+            # if z > 12 then thar means the year must change
+            if(months > 12):
+                if(not yearChange):
+                    year += 1
+                    yearChange = True
+                months = months % 12
+            # formats month value
+            monthVal = ""
+            if(months < 10):
+                monthVal = f"0{months}"
+            else:
+                monthVal = f"{months}"
 
-    for months in range(month, month+3):
-        # if z > 12 then thar means the year must change
-        if(months > 12):
-            if(not yearChange):
-                year += 1
-                yearChange = True
-            months = months % 12
-        # formats month value
-        monthVal = ""
-        if(months < 10):
-            monthVal = f"0{months}"
-        else:
-            monthVal = f"{months}"
+            # requests the url
+            response = requests.get(f'https://solecollector.com/sneaker-release-dates/{shoe_brand[brand]}/{year}/{monthVal}/')
+            soup = BeautifulSoup(response.content, 'html.parser')
 
-        # requests the url
-        response = requests.get(f'https://solecollector.com/sneaker-release-dates/{shoe_brand[brand]}/{year}/{monthVal}/')
-        soup = BeautifulSoup(response.content, 'html.parser')
+            # gets all the images and shoe data
+            releases = soup.find_all('div', class_="add-to-calendar")
+            release_images = soup.find_all('div', class_='sneaker-release__img-16x9')
+            release_prices = soup.find_all('span', class_='sneaker-release__option')
 
-        # gets all the images and shoe data
-        releases = soup.find_all('div', class_="add-to-calendar")
-        release_images = soup.find_all('div', class_='sneaker-release__img-16x9')
-        release_prices = soup.find_all('span', class_='sneaker-release__option')
+            # traverses the response and gets adds the name,data and image to a dictionary
+            for x in range(len(releases)):
+                # Removes color styles from shoe name
+                shoe_name = releases[x]['data-release-name']
+                if('/' in shoe_name):
+                    shoe_name = shoe_name[0 : shoe_name.find("/")][0:shoe_name.rfind(" ")]
+                # Appends the data to a dictionary list
 
-        # traverses the response and gets adds the name,data and image to a dictionary
-        for x in range(len(releases)):
-            # Removes color styles from shoe name
-            shoe_name = releases[x]['data-release-name']
-            if('/' in shoe_name):
-                shoe_name = shoe_name[0 : shoe_name.find("/")][0:shoe_name.rfind(" ")]
-            # Appends the data to a dictionary list
-
-            date = releases[x]['data-date'][:releases[x]['data-date'].rfind(' ')].split('/')
-
-            new_shoe = Shoe()
-            new_shoe.shoe_name = shoe_name
-            new_shoe.shoe_brand = brand
-            new_shoe.release_day = int(date[1])
-            new_shoe.release_month = int(date[0])
-            new_shoe.release_year = int("20" + str(date[2]))
-            new_shoe.release_time = releases[x]['data-date'][releases[x]['data-date'].rfind(' '):]
-            new_shoe.image = str(release_images[x].find_all('img', src=True)[0]['src'])
-            new_shoe.price = release_prices[x].text
-            new_shoe.hyped = isHyped(shoe_name)
-
-            if new_shoe not in Shoe.objects.all():
-                new_shoe.save()
+                date = releases[x]['data-date'][:releases[x]['data-date'].rfind(' ')].split('/')
+                
+                #To make sure that data is not duplicated
+                if Shoe.objects.filter(shoe_name= shoe_name).exists():
+                    continue
+                else:
+                    new_shoe = Shoe()
+                    new_shoe.shoe_name = shoe_name
+                    new_shoe.shoe_brand = brand
+                    new_shoe.release_day = int(date[1])
+                    new_shoe.release_month = int(date[0])
+                    new_shoe.release_year = int("20" + str(date[2]))
+                    new_shoe.release_time = releases[x]['data-date'][releases[x]['data-date'].rfind(' '):]
+                    new_shoe.image = str(release_images[x].find_all('img', src=True)[0]['src'])
+                    new_shoe.price = release_prices[x].text
+                    new_shoe.hyped = isHyped(shoe_name)
+                    new_shoe.save()
 
 def isHyped(shoe_name):
     """Checked if shoe is hyped or not"""
--- shoe_tab/models.py
+++ shoe_tab/models.py
@@ -1,13 +1,16 @@
+
 from django.db import models
+
+
 
 class Shoe(models.Model):
     """The Shoe class for acessing and putting in new shoe data"""
-    shoe_brand = models.CharField(max_length=16)    # hoe brand for filtering
-    shoe_name = models.CharField(max_length=128)    # name of the shoe
-    release_day = models.IntegerField()             # release day field
-    release_month = models.IntegerField()           # release month field
-    release_year = models.IntegerField()            # release year field
-    release_time = models.CharField(max_length=16)  # release time
-    image = models.URLField()                       # image url
-    price = models.CharField(max_length=16)         # drop price
-    hyped = models.BooleanField()                   # if shoe is hyped or not
+    shoe_brand = models.CharField(max_length=16)                # shoe brand for filtering
+    shoe_name = models.CharField(max_length=128, unique=True)   # name of the shoe
+    release_day = models.IntegerField()                         # release day field
+    release_month = models.IntegerField()                       # release month field
+    release_year = models.IntegerField()                        # release year field
+    release_time = models.CharField(max_length=16)              # release time
+    image = models.URLField()                                   # image url
+    price = models.CharField(max_length=16)                     # drop price
+    hyped = models.BooleanField()                               # if shoe is hyped or not
